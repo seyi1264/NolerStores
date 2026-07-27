2486354e6ca99a39430b1a018e6ac908752b70e4
seyi1264 <seyi1264@gmail.com>
2026-07-27 15:44:48 +0100

    Update Paystack public key in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NOLER_CONFIG = {
   API_BASE_URL: 'https://nolerstores-xwlgba.fly.dev',
-  PAYSTACK_PUBLIC_KEY: 'pk_test_your_public_key',
+  PAYSTACK_PUBLIC_KEY: 'sk_live_25c053bc8a576f3e3ad30181b2d5d7df273affdc',
   // A timestamp to force redeploys to pick up changes when needed
   DEPLOYED_AT: '2026-07-27T14:40:00Z',
 };
